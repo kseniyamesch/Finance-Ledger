87edaf99dc3c8db2768add1391735f847bee7e03
Kseniia <kseniyamesch0101@gmail.com>
2023-04-29 16:14:27 +0300

    add sticky header

--- src/index.js
+++ src/index.js
@@ -14,3 +14,11 @@ for (let anchor of anchors) {
     })
   })
 }
+
+window.addEventListener('scroll', function() {
+    if (pageYOffset > 50) {
+      document.querySelector('.header').classList.add('active')
+    } else {
+      document.querySelector('.header').classList.remove('active')
+    }
+  })
